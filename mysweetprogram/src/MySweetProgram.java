7513f90d5837212dfcc90622adcacbe792b47c5a
TAI DO <duc4@e.ntu.edu.sg>
2023-12-22 14:14:55 +0800

    iterator

--- mysweetprogram/src/MySweetProgram.java
+++ mysweetprogram/src/MySweetProgram.java
@@ -1,8 +1,10 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.Scanner;
 
 public class MySweetProgram {
@@ -11,23 +13,23 @@ public class MySweetProgram {
 	public static void main(String[] args) throws FileNotFoundException {
 		
 		// input from console
+		
 		// LinkedList as a queue -> first in first out
 		LinkedList<String> names= new LinkedList<String>();
 		names.push("Caleb");
 		names.push("Sue");
 		names.push("Sally");
 		
-		System.out.println(names.remove(1));
-		System.out.println(names.pop());
-		System.out.println(names.pop());
+		ListIterator<String> it=names.listIterator();
+		it.next();
+		it.next();
+		it.add("Susan");
 		
-				
 		
 		
+		for (String name:names) System.out.println(name);
 		
 	}
-	
-	
 }
 
 // class -> instantiate -> object
